afc04ec64639161306cd49f65c3218717b3297d1
Dennis Schultz <dennis.schultz@us.ibm.com>
2015-10-14 18:15:47 -0400

    Correct variant of app.js

--- IBMTechEXEmployee/www/js/app.js
+++ IBMTechEXEmployee/www/js/app.js
@@ -4,212 +4,210 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 
-    var ibmApp = angular.module('ibmApp', ['ionic'])
-    
+var ibmApp = angular.module('ibmApp', ['ionic'])
     //Adding support for cordova.
-    ibmApp.run(function($ionicPlatform) {
-          console.log('>> ibmApp.run ...');
-          $ionicPlatform.ready(function() {
-            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-            // for form inputs)
-            console.log('>> ibmApp.ready ...');  
-            if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-              cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-            }
-            if(window.StatusBar) {
-              StatusBar.styleDefault();
-            }
-          });
-        });
-    
-    //appliction config
-    ibmApp.config(function($stateProvider, $urlRouterProvider) {
-      // $urlRouterProvider - letting us specifsy the default route when loading the module    
-      $urlRouterProvider.otherwise('/')
-      $stateProvider                    
-          .state('main', {
+ibmApp.run(function ($ionicPlatform) {
+    console.log('>> ibmApp.run ...');
+    $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        console.log('>> ibmApp.ready ...');
+        if (window.cordova &&
+            window.cordova.plugins &&
+            window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+});
+
+//application config
+ibmApp.config(function ($stateProvider, $urlRouterProvider) {
+    // $urlRouterProvider - letting us specifsy the default route when loading the module    
+    $urlRouterProvider.otherwise('/')
+    $stateProvider
+        .state('main', {
             url: '/main',
             templateUrl: 'partials/employee.html',
-            controller: 'mainCtrl'            
-            })          
-          .state('login', {
-            url: '/', /* default url */
+            controller: 'mainCtrl'
+        })
+        .state('login', {
+            url: '/',
+            /* default url */
             templateUrl: 'pages/login.html',
             controller: 'loginCtrl'
-            
-            })                      
-          .state('detail', {            
-            url: '/:empId',            
-            templateUrl: 'partials/details.html',                   
+
+        })
+        .state('detail', {
+            url: '/detail/:empId',
+            templateUrl: 'partials/details.html',
             controller: 'employeeDetailCtrl',
-            resolve:{  
-                employee:  function($stateParams, EmployeeDetailsService){                        
-                            return {
-                                _id: $stateParams.empId,
-                                employeeDetailsService : EmployeeDetailsService
-                            };                        
-                    }
-            }})                    
-    })
-    
-    //appliction services for employee and employee details.
-    ibmApp.factory("EmployeeService", function($http){
-        console.log( ">> in EmployeeService ...");  
-        var employees = [];
-        return {                        
-            getEmployeeList: function(){
-                return $http.get('data/employee.json').then(function(response){
-                    employees = response.data;
-                    return employees;
-                });
-            },
-            
-            getEmployee: function(index){
-                return employees[index];
-            },
-            
-            getEmployeeById: function(id){
-                var _emp;
-                console.log(">> getEmployeeById :" + id);
-                angular.forEach(employees, function(emp) {
-                    console.log(">> getEmployeeById :" + id + " ==  " + emp._id );
-                    if(emp._id == id){                        
-                        _emp = emp;
-                    }   
-                });
-                return _emp;
-                
+            resolve: {
+                employeeDetailList: function ($stateParams, EmployeeDetailsService) {
+                    return EmployeeDetailsService.getEmployeeDetails($stateParams.empId);
+                },
+                empId: function ($stateParams) {
+                    return $stateParams.empId;
+                }
             }
-        };    
-    })
-    
-    ibmApp.factory("EmployeeDetailsService", function($http){
-        console.log( ">> in EmployeeDetailsService ...");        
-        return {            
-            getEmployeeDetails: function(empId){
-                return $http.get('data/details.json');
-            }};    
-    })
-    
-    
-    // Application controllers.
-    
-    ibmApp.controller('appCtrl', function($scope){    
-        $scope.logout = function(){
-            console.log(">> in appCtrl >> logout ... ");
-            $scope.user = {
-                username: "",
-                password: ""
-            };            
-        }        
-    })
-    
-    
-    ibmApp.controller('mainCtrl', [ '$scope', 'EmployeeService', function($scope, EmployeeService) {
-        console.log(">> in mainCtrl ... ");                
-        $scope.$on('$ionicView.beforeEnter', function(){
-            console.log(">> mainCtrl beforeEnter");            
-        });        
-        EmployeeService.getEmployeeList().then(function(rsp){
-            console.log(">> EmployeeService.getEmployeeList() -> then :" + rsp);            
-                $scope.employees = rsp;
-        });        
-    }])
-    
-        
-    ibmApp.controller('employeeDetailCtrl', function($scope, EmployeeService,  EmployeeDetailsService, employee , $ionicHistory) {
-        $scope.employee = {
-            "first_name" : "", 
-            "last_name" : "",
-            "_id" : ""
+        })
+})
+
+//application services for employee and employee details.
+ibmApp.factory("EmployeeService", function ($http) {
+    console.log(">> in EmployeeService ...");
+    var employees = [];
+    return {
+        getEmployeeList: function () {
+            return $http.get('data/employee.json').then(function (response) {
+                employees = response.data;
+                return employees;
+            });
+        },
+
+        getEmployee: function (index) {
+            return employees[index];
+        },
+
+        getEmployeeById: function (id) {
+            var _emp;
+            console.log(">> getEmployeeById :" + id);
+            angular.forEach(employees, function (emp) {
+                console.log(">> getEmployeeById :" + id + " ==  " + emp._id);
+                if (emp._id == id) {
+                    _emp = emp;
+                }
+            });
+            return _emp;
+
         }
-        $scope.employeeDetails = {}
-        console.log(">> in - employeeDetailCtrl:" + employee);
-        $scope.employee = employee;
-        $scope.employee = EmployeeService.getEmployeeById(employee._id);
-        EmployeeDetailsService.getEmployeeDetails(employee._id).then(
-            function(rsp){
-                console.log(">> in - employeeDetailCtrl -> rsp:" + rsp);                         
-                angular.forEach(rsp.data, function(emp) {                    
-                    if(emp._id == employee._id){                        
-                        $scope.employeeDetails = emp;
-                        $scope.employeeDetails.email =  angular.lowercase($scope.employeeDetails.email);
-                    }   
-                });
-                
-            }
-        );
-    })
-    
-        
-    ibmApp.controller('loginCtrl', [ '$scope', '$state', '$timeout', 
-                                    function($scope, $state, $timeout) {        
+    };
+})
+
+ibmApp.factory("EmployeeDetailsService", function ($http) {
+    console.log(">> in EmployeeDetailsService ...");
+    return {
+        getEmployeeDetails: function (empId) {
+            return $http.get('data/details.json');
+        }
+    };
+})
+
+
+// Application controllers.
+
+ibmApp.controller('appCtrl', function ($scope) {
+    $scope.logout = function () {
+        console.log(">> in appCtrl >> logout ... ");
+        $scope.user = {
+            username: "",
+            password: ""
+        };
+    }
+})
+
+
+ibmApp.controller('mainCtrl', ['$scope', 'EmployeeService', function ($scope, EmployeeService) {
+    console.log(">> in mainCtrl ... ");
+    $scope.$on('$ionicView.beforeEnter', function () {
+        console.log(">> mainCtrl beforeEnter");
+    });
+    EmployeeService.getEmployeeList().then(function (rsp) {
+        console.log(">> EmployeeService.getEmployeeList() -> then :" + rsp);
+        $scope.employees = rsp;
+    });
+    }])
+
+
+ibmApp.controller('employeeDetailCtrl', function ($scope, EmployeeService,
+    employeeDetailList, empId, $ionicHistory) {
+    $scope.employee = {
+        "first_name": "",
+        "last_name": "",
+        "_id": ""
+    }
+    $scope.employeeDetails = {}
+    console.log(">> in - employeeDetailCtrl:" + employeeDetailList);
+    //Employee service cached the list of employee
+    $scope.employee = EmployeeService.getEmployeeById(empId);
+    var data = employeeDetailList.data;
+    angular.forEach(data, function (emp) {
+        if (emp._id == $scope.employee._id) {
+            $scope.employeeDetails = emp;
+            $scope.employeeDetails.email = angular.lowercase($scope.employeeDetails.email);
+        }
+    });
+})
+
+
+ibmApp.controller('loginCtrl', ['$scope', '$state', '$timeout',
+                                    function ($scope, $state, $timeout) {
         console.log(">> loginCtrl - $state.current:" + $state.current);
         $scope.user = {
             username: "",
             password: ""
-        }       
-                         
-        $scope.doLogin = function(){
+        }
+
+        $scope.doLogin = function () {
             console.log(">> loginCtrl - doLogin() ... ");
             console.log(">> loginCtrl - $scope.user:" + $scope.user);
-            $state.transitionTo("main");                            
+            $state.transitionTo("main");
         }
-                        
-        $scope.moveLoginBox = function() {
+
+        $scope.moveLoginBox = function () {
             var loginBox = document.getElementById('login-box');
             move(loginBox).ease('in-out').y(-385).duration('0.5s').end();
             move('.signInMsg').rotate(360).end();
         };
-        
-        $timeout(function(){
+
+        $timeout(function () {
             $scope.moveLoginBox();
         }, 200);
     }])
-    
-    
-    
-    //Adding MobileFirst code.
-    var Messages = {
-      // Add here your messages for the default language.
-      // Generate a similar file with a language suffix containing the translated messages.
-      // key1 : message1,
-    };
 
-    var wlInitOptions = {
-        // Options to initialize with the WL.Client object.
-        // For initialization options please refer to IBM MobileFirst Platform Foundation Knowledge Center.
-    };
+//Adding MobileFirst
+var Messages = {
+    // Add here your messages for the default language.
+    // Generate a similar file with a language suffix containing the translated messages.
+    // key1 : message1,
+};
 
-    // Called automatically after MFP framework initialization by WL.Client.init(wlInitOptions).
-    function wlCommonInit(){
-        // Common initialization code goes here
-      console.log('>> MobileFirst Client SDK Initilized ...');
-      angular.element(document).ready(function() {
+var wlInitOptions = {
+    // Options to initialize with the WL.Client object.
+    // For initialization options please refer to IBM MobileFirst Platform Foundation Knowledge Center.
+};
+
+// Called automatically after MFP framework initialization by WL.Client.init(wlInitOptions).
+function wlCommonInit() {
+    // Common initialization code goes here
+    console.log('MobileFirst Client SDK Initilized');
+    angular.element(document).ready(function () {
         mfpMagicPreviewSetup();
         angular.bootstrap(document.body, ['ibmApp']);
-      });
-    }
+    });
+}
 
-    function mfpMagicPreviewSetup(){
-      //nothing to see here :-), just some magic to make ionic work with mfp preview, similar to ionic serve --lab
-      if(typeof WL !== 'undefined' && WL.StaticAppProps && WL.StaticAppProps.ENVIRONMENT === 'preview'){
+function mfpMagicPreviewSetup() {
+    //nothing to see here :-), just some magic to make ionic work with mfp preview, similar to ionic serve --lab
+    if (typeof WL !== 'undefined' && WL.StaticAppProps && WL.StaticAppProps.ENVIRONMENT === 'preview') {
         //running mfp preview (MBS or browser)
-        if(WL.StaticAppProps.PREVIEW_ENVIRONMENT === 'android'){
-          document.body.classList.add('platform-android');
-          ionic.Platform.setPlatform("android");
+        if (WL.StaticAppProps.PREVIEW_ENVIRONMENT === 'android') {
+            document.body.classList.add('platform-android');
+            ionic.Platform.setPlatform("android");
         } else { //then ios
-          document.body.classList.add('platform-ios');
-          ionic.Platform.setPlatform("ios");
+            document.body.classList.add('platform-ios');
+            ionic.Platform.setPlatform("ios");
         }
-        } 
     }
+}
 
-    // Useful for ionic serve when MFP Client SDK is not present and wlCommonInit doesn't get called automatically
-    var serveTimeout = 1500;
-    window.setTimeout(function(){
-      if(typeof WL === 'undefined'){
-          console.log('>> MFP Client SDK timeout, running Web App ...');
-          angular.bootstrap(document.body, ['ibmApp']);
-      }
-    }, serveTimeout);
+// Useful for ionic serve when MFP Client SDK is not present and wlCommonInit doesn't get called automatically
+var serveTimeout = 1500;
+window.setTimeout(function () {
+    if (typeof WL === 'undefined') {
+        console.log('MFP Client SDK timeout, running Web App');
+        angular.bootstrap(document.body, ['guardianApp']);
+    }
+}, serveTimeout);
