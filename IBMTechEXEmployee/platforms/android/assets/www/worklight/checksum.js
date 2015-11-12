473d259938c85ba799330630075161e11bd351cd
dschultz-mo <dennis.schultz@us.ibm.com>
2015-11-11 19:35:22 -0600

    Merge branch 'step-4-updates' into step-5-updates

--- IBMTechEXEmployee/platforms/android/assets/www/worklight/checksum.js
+++ IBMTechEXEmployee/platforms/android/assets/www/worklight/checksum.js
@@ -1,2 +1,7 @@
+<<<<<<< HEAD
 var WL_CHECKSUM = {"checksum":165980445,"date":1444944624961,"machine":"ibm-virtual-machine"}
 /* Date: Thu Oct 15 2015 17:30:24 GMT-0400 (EDT) */
+=======
+var WL_CHECKSUM = {"checksum":2320060367,"date":1441299066767,"machine":"Elirans-MacBook-Pro.local"}
+/* Date: Thu Sep 03 2015 12:51:06 GMT-0400 (EDT) */
+>>>>>>> refs/heads/step-4-updates
